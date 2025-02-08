7a3f76aa56855418782c33b745841a81d52a0f44
HamzaAatar <aatar38@gmail.com>
2025-02-08 23:18:42 +0100

    vite new config and fixes

---
+++ vite.config.js
@@ -0,0 +1,36 @@
+import { defineConfig } from "vite"
+import { resolve } from "path"
+
+export default defineConfig({
+	root: "./",
+	build: {
+		outDir: "dist",
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, "index.html"),
+				dark: resolve(__dirname, "index-dark.html"),
+			},
+			external: ["jquery", "skrollr"],
+			output: {
+				assetFileNames: (assetInfo) => {
+					const info = assetInfo.name.split(".")
+					const extType = info[info.length - 1]
+					if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+						return `assets/img/[name][extname]`
+					}
+					return `assets/${extType}/[name][extname]`
+				},
+				entryFileNames: `assets/js/[name].js`,
+				chunkFileNames: `assets/js/[name].js`,
+				manualChunks: undefined,
+			},
+		},
+		assetsInlineLimit: 0,
+		copyPublicDir: true,
+	},
+	publicDir: "public",
+	server: {
+		open: true,
+		port: 3000,
+	},
+})
